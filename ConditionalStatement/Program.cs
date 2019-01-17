4cccaa5ab8c0efa4a2bea90866bad14cb2d39f43
Gregory Gauthier <talonk165@hotmail.com>
2019-01-17 17:53:14 -0500

    This is a fresh Code To Check Grades

--- ConditionalStatement/Program.cs
+++ ConditionalStatement/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ Author: Gregory Gauthier
+ Date: 1/17/2019
+ Comments: This C# Console application code demonstrates the use 
+ of conditional statements after getting input from users
+ */
+
+
+using System;
 
 namespace ConditionalStatement
 {
@@ -6,7 +14,45 @@ namespace ConditionalStatement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please Enter The Grade you think You Will Earn in ISM 4300");
+
+            /*
+             Here we will be using a try-catch block 
+             In order to validate the users input.
+             This will handle any errors and display a message.
+             */
+
+            try
+            {
+                //this variable will be gathering the users input
+
+                string UserInput = Console.ReadLine();
+                int ExpectedGrade = int.Parse(UserInput);
+
+                if ((ExpectedGrade > 80) && (ExpectedGrade < 90))
+                    Console.WriteLine("You Will Earn a B");
+
+                else if (ExpectedGrade > 90)
+                    Console.WriteLine("You Will Earn an A");
+
+                else if ((ExpectedGrade < 80) && (ExpectedGrade > 70))
+                    Console.WriteLine("You Will Earn a C");
+                else if (ExpectedGrade < 70)
+                    Console.WriteLine("You Will Earn a D");
+
+                Console.ReadKey(true);
+            }
+
+            catch
+            {
+                Console.WriteLine("Please make sure to use a Number Value for your expected grade");
+
+            }
+
+            
+
+                    
+            
         }
     }
 }
